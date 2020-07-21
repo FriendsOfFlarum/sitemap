3615d46e3e80b4b8e585a00a83ecc62812b695da
Eddie Webbinaro <ollitech@gmail.com>
2020-07-21 17:47:26 -0400

    Add debug logs to register
    shows we add our resource and increase total resource count - I dont know where this returns it to.
    
    added logging to generator
    
    Fixes #14 Use container->extend instead of resolv
    
    I dont know if it is a pass by reference/copy issue, but modifying the array within a resolving callback was ignored (see issue #14)
    but per illumiante docs we can wrap and augment existing intances using extend.
    
    I left debug logging in place to see differec.
    
    linting
    
    linting
    
    linting
    
    linting
    
    linting
    
    Update SitemapGenerator.php

--- src/Extend/RegisterResource.php
+++ src/Extend/RegisterResource.php
@@ -32,14 +32,18 @@ class RegisterResource implements ExtenderInterface
 
     public function extend(Container $container, Extension $extension = null)
     {
-        $container->resolving('fof.sitemap.resources', function (array $resources) use ($container) {
+        $container->extend('fof.sitemap.resources', function (array $resources) use ($container) {
             $resource = $container->make($this->resource);
 
             if ($resource instanceof Resource) {
                 $resources[] = $resource;
+                // confirms extension is added here.
+                app('log')->debug('[RegisterResource::extend] added '.get_class($resource));
             } else {
                 throw new InvalidArgumentException("{$this->resource} has to extend ".Resource::class);
             }
+            // here on extension register poiint we correctly see all core + extension resources.
+            app('log')->debug('[RegisterResource::extend] resolved '.count($resources).' resources.');
 
             return $resources;
         });
