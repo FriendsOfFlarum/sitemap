45c6b31ac14b57ca4f46f5085da7cc17049ffc72
David Wheatley <davwheat@users.noreply.github.com>
2021-03-28 02:14:14 +0000

    Apply fixes from StyleCI

--- extend.php
+++ extend.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of fof/sitemap.
  *
- * Copyright (c) 2021 FriendsOfFlarum.
+ * Copyright (c) 2020 FriendsOfFlarum.
  *
  *  For the full copyright and license information, please view the LICENSE.md
  *  file that was distributed with this source code.
