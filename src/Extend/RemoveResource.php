abf7e00ee4fd145dd1b7b25e0accce2e75fd9065
Rafael Horvat <rafael.horvat@icloud.com>
2021-09-30 15:58:01 +0200

    Added `RemoveResource` extender. (#34)
    * Added `RemoveResource` extender.
    
    * Added docs for removing a resource to README

---
+++ src/Extend/RemoveResource.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of fof/sitemap.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ */
+
+namespace FoF\Sitemap\Extend;
+
+use Flarum\Extend\ExtenderInterface;
+use Flarum\Extension\Extension;
+use FoF\Sitemap\Resources\Resource;
+use Illuminate\Contracts\Container\Container;
+use InvalidArgumentException;
+
+class RemoveResource implements ExtenderInterface
+{
+    /**
+     * @var string
+     */
+    private $resource;
+
+    public function __construct(string $resource)
+    {
+        $this->resource = $resource;
+    }
+
+    public function extend(Container $container, Extension $extension = null)
+    {
+        $container->extend('fof.sitemap.resources', function (array $resources) use ($container) {
+            $resource = $container->make($this->resource);
+
+            if ($resource instanceof Resource) {
+                $resources = array_filter($resources, function ($res) {
+                    return get_class($res) !== $this->resource;
+                });
+            } else {
+                throw new InvalidArgumentException("{$this->resource} has to extend ".Resource::class);
+            }
+
+            return $resources;
+        });
+    }
+}
