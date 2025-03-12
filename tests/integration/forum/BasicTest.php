f9fee36e0c566528fdc7d46b9784ea3e38502c41
IanM <ian@morland.me>
2025-03-12 07:56:03 +0000

    wip: add basic sanity test for each mode

---
+++ tests/integration/forum/BasicTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace FoF\Sitemap\Tests\integration\forum;
+
+use Flarum\Testing\integration\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->extension('fof-sitemap');
+    }
+
+    /**
+     * @test
+     */
+    public function sitemap_is_available_in_runtime_mode()
+    {
+        $response = $this->send(
+            $this->request('GET', '/sitemap.xml')
+        );
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">', $response->getBody()->getContents());
+    }
+
+    /**
+     * @test
+     */
+    public function sitemap_is_available_in_muti_file_mode()
+    {
+        $this->setting('fof-sitemap.mode', 'multi-file');
+
+        $response = $this->send(
+            $this->request('GET', '/sitemap.xml')
+        );
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertStringContainsString('<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">', $response->getBody()->getContents());
+    }
+}
