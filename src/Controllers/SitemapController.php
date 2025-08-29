70f65597ff0debea85a38d377107596798b8be69
IanM <ian@morland.me>
2025-08-29 13:28:02 +0100

    chore: use dep inj

--- src/Controllers/SitemapController.php
+++ src/Controllers/SitemapController.php
@@ -20,43 +20,43 @@ use Laminas\Diactoros\Response;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Psr\Log\LoggerInterface;
 
 class SitemapController implements RequestHandlerInterface
 {
     public function __construct(
         protected DeployInterface $deploy,
         protected SettingsRepositoryInterface $settings,
-        protected Generator $generator
+        protected Generator $generator,
+        protected LoggerInterface $logger
     ) {
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        $logger = resolve('log');
-
         // Get route parameters from the request attributes
         $routeParams = $request->getAttribute('routeParameters', []);
         $id = $routeParams['id'] ?? null;
 
-        $logger->debug('[FoF Sitemap] Route parameters: '.json_encode($routeParams));
-        $logger->debug('[FoF Sitemap] Extracted ID: '.($id ?? 'null'));
+        $this->logger->debug('[FoF Sitemap] Route parameters: '.json_encode($routeParams));
+        $this->logger->debug('[FoF Sitemap] Extracted ID: '.($id ?? 'null'));
 
         if ($id !== null) {
             // Individual sitemap request
-            $logger->debug("[FoF Sitemap] Handling individual sitemap request for set: $id");
+            $this->logger->debug("[FoF Sitemap] Handling individual sitemap request for set: $id");
 
             if ($this->deploy instanceof Memory) {
-                $logger->debug('[FoF Sitemap] Memory deployment: Generating sitemap on-the-fly');
+                $this->logger->debug('[FoF Sitemap] Memory deployment: Generating sitemap on-the-fly');
                 $this->generator->generate();
             }
 
             $content = $this->deploy->getSet($id);
         } else {
             // Index request
-            $logger->debug('[FoF Sitemap] Handling sitemap index request');
+            $this->logger->debug('[FoF Sitemap] Handling sitemap index request');
 
             if ($this->deploy instanceof Memory) {
-                $logger->debug('[FoF Sitemap] Memory deployment: Generating sitemap on-the-fly');
+                $this->logger->debug('[FoF Sitemap] Memory deployment: Generating sitemap on-the-fly');
                 $this->generator->generate();
             }
 
@@ -64,12 +64,12 @@ class SitemapController implements RequestHandlerInterface
         }
 
         if (is_string($content) && !empty($content)) {
-            $logger->debug('[FoF Sitemap] Successfully serving sitemap content');
+            $this->logger->debug('[FoF Sitemap] Successfully serving sitemap content');
 
             return new Response\XmlResponse($content);
         }
 
-        $logger->debug('[FoF Sitemap] No sitemap content found, returning 404');
+        $this->logger->debug('[FoF Sitemap] No sitemap content found, returning 404');
 
         return new Response\EmptyResponse(404);
     }
