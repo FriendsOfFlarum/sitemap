1124df6e6282be8f8cb37888c48814cf372fd2b0
Grimur Vid Neyst <grimur.vid-neyst@glowingblue.com>
2025-12-10 10:45:03 +0100

    sitemap-xrobot-fix: Middleware added to add header tag X-Robots-Tag
    1. Created a middleware folder and a ApiRobotsHeader file
    2. In the extension the middleware file is added which hints that noindexing should be done on api's
    
    Reported by: Grimur Vid Neyst
    Signed off by: Grimur Vid Neyst <grimur.vid-neyst@glowingblue.com>

---
+++ src/Middleware/ApiRobotsHeader.php
@@ -0,0 +1,25 @@
+<?php
+    namespace FoF\Sitemap\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class ApiRobotsHeader implements MiddlewareInterface
+{
+    public function __construct(private string $value = 'noindex, nofollow')
+    {
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+
+        if (!$response->hasHeader('X-Robots-Tag')) {
+            $response = $response->withHeader('X-Robots-Tag', $this->value);
+        }
+
+        return $response;
+    }
+}
