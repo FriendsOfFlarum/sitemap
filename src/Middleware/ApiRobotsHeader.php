067af62db80850855fbd474ffe5d3d6ddc06e1ee
Grimur Vid Neyst <grimur.vid-neyst@glowingblue.com>
2025-12-10 10:49:16 +0100

    Style fixes

--- src/Middleware/ApiRobotsHeader.php
+++ src/Middleware/ApiRobotsHeader.php
@@ -1,5 +1,16 @@
 <?php
-    namespace FoF\Sitemap\Middleware;
+
+/*
+ * This file is part of fof/sitemap.
+ *
+ * Copyright (c) FriendsOfFlarum.
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ *
+ */
+
+namespace FoF\Sitemap\Middleware;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
